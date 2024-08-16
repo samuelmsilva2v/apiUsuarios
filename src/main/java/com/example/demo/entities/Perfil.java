e88f9948c6593689431d2f1836e1a885bf4f803b
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-08-16 17:25:45 -0300

    jpa config added, annotatios added to entities

--- src/main/java/com/example/demo/entities/Perfil.java
+++ src/main/java/com/example/demo/entities/Perfil.java
@@ -1,13 +1,28 @@
 package com.example.demo.entities;
 
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "tb_perfil")
 @Data
 public class Perfil {
 
+	@Id
+	@Column(name = "id")
 	private UUID id;
+
+	@Column(name = "nome", length = 50, nullable = false)
 	private String nome;
+	
+	@OneToMany(mappedBy = "perfil")
+	private List<Usuario> usuarios;
 
 }
--- src/main/java/com/example/demo/entities/Usuario.java
+++ src/main/java/com/example/demo/entities/Usuario.java
@@ -2,13 +2,33 @@ package com.example.demo.entities;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "tb_usuario")
 @Data
 public class Usuario {
 
+	@Id
+	@Column(name = "id")
 	private UUID id;
+	
+	@Column(name = "nome", length = 100, nullable = false)
 	private String nome;
+	
+	@Column(name = "email", length = 50, nullable = false, unique = true)
 	private String email;
+	
+	@Column(name = "senha", length = 100, nullable = false)
 	private String senha;
+	
+	@ManyToOne
+	@JoinColumn(name = "perfil_id", nullable = false)
+	private Perfil perfil;
 }
