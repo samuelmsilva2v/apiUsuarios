cc66ff73bc6c88af71eb2b49c099d6aa92319e29
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-08-27 16:56:32 -0300

    classes updated

--- src/main/java/com/example/demo/components/RabbitMQProducerComponent.java
+++ src/main/java/com/example/demo/components/RabbitMQProducerComponent.java
@@ -3,7 +3,6 @@ package com.example.demo.components;
 import java.util.Date;
 import java.util.UUID;
 
-import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Component;
 import com.example.demo.collections.LogMensageria;
 import com.example.demo.dtos.MensagemUsuarioResponse;
 import com.example.demo.repositories.LogMensageriaRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
